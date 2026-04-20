bd6e5a1db03182d9beb60c86f2de9b2dcfd6a705
nathalyapereira <nathalyperreira741@gmail.com>
2026-04-20 01:34:40 -0300

    feat: Implementando entidades, service, controller e dto

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('api-docs', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
@@ -35,8 +35,6 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 
